9ac86cb234c181f2b74afec37c6c23e0b2a66289
geniushub <prajapatig2411@gmail.com>
2022-02-16 18:43:30 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
 
 function App() {
+  const click = () => 
+  {
+    alert('hello')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+      color='secondary'
+        onClick={() => { click() }}
+        startIcon={<Delete/>}>
+        hello
+      </Button>
     </div>
   );
 }
